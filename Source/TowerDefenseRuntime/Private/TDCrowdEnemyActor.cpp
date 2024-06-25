492ccda6ff9c36a4f713860469053b6ce1692c39
Evolto-Z <15652575953@163.com>
2024-06-26 00:24:18 +0800

    first commit

---
+++ Source/TowerDefenseRuntime/Private/TDCrowdEnemyActor.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TDCrowdEnemyActor.h"
+
+#include "MassAgentComponent.h"
+
+
+// Sets default values
+ATDCrowdEnemyActor::ATDCrowdEnemyActor()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	AgentComponent = CreateDefaultSubobject<UMassAgentComponent>(TEXT("MassAgent"));
+}
+
+// Called when the game starts or when spawned
+void ATDCrowdEnemyActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ATDCrowdEnemyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ATDCrowdEnemyActor::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/TowerDefenseRuntime/Private/TDCrowdEnemyActor.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MassActorPoolableInterface.h"
+#include "GameFramework/Character.h"
+#include "TDCrowdEnemyActor.generated.h"
+
+class UMassAgentComponent;
+
+UCLASS()
+class TOWERDEFENSERUNTIME_API ATDCrowdEnemyActor : public ACharacter, public IMassActorPoolableInterface
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ATDCrowdEnemyActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UMassAgentComponent> AgentComponent;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
